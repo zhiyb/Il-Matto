9fe1c3cb86de8f386419077b20035cab5e0fdab4
zhiyb <yz39g13@soton.ac.uk>
2014-07-14 14:19:37 +0100

    ATMEGA168P USBasp

--- USBasp/firmware/bootloaderconfig.h
+++ USBasp/firmware/bootloaderconfig.h
@@ -42,7 +42,7 @@ these macros are defined, the boot loader usees them.
 /* This is the port where the USB bus is connected. When you configure it to
  * "B", the registers PORTB, PINB and DDRB will be used.
  */
-#define USB_CFG_DMINUS_BIT      2
+#define USB_CFG_DMINUS_BIT      4
 /* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
  * This may be any bit in the port.
  */
