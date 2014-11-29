400356a950c39849c9cb627cd15dec5941f7a099
zhiyb <yz39g13@soton.ac.uk>
2014-11-29 22:31:30 +0000

    Rename TFT to TFT-CPP, TFT-C to TFT

--- TFT-CPP/conv.h
+++ TFT-CPP/conv.h
@@ -1,10 +1,24 @@
 #ifndef RGBCONV_H
 #define RGBCONV_H
 
-#include <inttypes.h>
+#include <stdio.h>
+
+namespace conv
+{
+	static inline uint16_t c16i(uint16_t c16);
+	static inline uint32_t c32i(uint32_t c32);
+	static inline uint16_t c32to16(uint32_t c32);
+	static inline uint16_t c32to16i(uint32_t c32);
+
+	static inline uint32_t uint24(uint8_t block[3]);
+	static inline uint32_t uint32(uint8_t block[4]);
+
+	static inline uint32_t uint24(FILE *f);
+	static inline uint32_t uint32(FILE *f);
+}
 
 // Inline functions
-static inline uint16_t conv_c16i(uint16_t c16)
+static inline uint16_t conv::c16i(uint16_t c16)
 {
 	// 16-bit: rrrr rggg gggb bbbb
 	return ((c16 & 0xF800) >> 11) + \
@@ -12,14 +26,14 @@ static inline uint16_t conv_c16i(uint16_t c16)
 		((c16 & 0x001F) << 11);
 }
 
-static inline uint32_t conv_c32i(uint32_t c32)
+static inline uint32_t conv::c32i(uint32_t c32)
 {
 	return ((c32 & 0x00FF0000) >> 16) + \
 		(c32 & 0x0000FF00) + \
 		((c32 & 0x000000FF) << 16);
 }
 
-static inline uint16_t conv_c32to16(uint32_t c32)
+static inline uint16_t conv::c32to16(uint32_t c32)
 {
 	// 16-bit: rrrr rggg gggb bbbb
 	return ((c32 & 0x00F80000) >> 8) + \
@@ -27,19 +41,19 @@ static inline uint16_t conv_c32to16(uint32_t c32)
 		((c32 & 0x000000F8) >> 3);
 }
 
-static inline uint16_t conv_c32to16i(uint32_t c32)
+static inline uint16_t conv::c32to16i(uint32_t c32)
 {
 	return c32to16(c32i(c32));
 }
 
-static inline uint32_t conv_uint24(uint8_t block[3])
+static inline uint32_t conv::uint24(uint8_t block[3])
 {
 	return (uint32_t)block[0] | \
 		((uint32_t)block[1] * 0x0100) | \
 		((uint32_t)block[2] * 0x00010000);
 }
 
-static inline uint32_t conv_uint32(uint8_t block[4])
+static inline uint32_t conv::uint32(uint8_t block[4])
 {
 	return (uint32_t)block[0] | \
 		((uint32_t)block[1] * 0x0100) | \
@@ -47,7 +61,7 @@ static inline uint32_t conv_uint32(uint8_t block[4])
 		((uint32_t)block[3] * 0x01000000);
 }
 
-static inline uint32_t conv_uint24(FILE *f)
+static inline uint32_t conv::uint24(FILE *f)
 {
 	uint32_t res = (uint8_t)fgetc(f);
 	res |= (uint32_t)(uint8_t)fgetc(f) * 0x0100;
@@ -55,7 +69,7 @@ static inline uint32_t conv_uint24(FILE *f)
 	return res;
 }
 
-static inline uint32_t conv_uint32(FILE *f)
+static inline uint32_t conv::uint32(FILE *f)
 {
 	uint32_t res = (uint8_t)fgetc(f);
 	res |= (uint32_t)(uint8_t)fgetc(f) * 0x0100;
