33363193cbaa2d58598e0338cd957db9c89d4cc6
zhiyb <yz39g13@soton.ac.uk>
2014-12-24 01:15:54 +0000

    OV7670 PORTA & PORTC connection

--- OV7670/libOV7670-cpp_AC/connection.h
+++ OV7670/libOV7670-cpp_AC/connection.h
@@ -2,10 +2,10 @@
 #define LIBOV7670_CONNECTION_H
 
 // OV7670 (CF7670C-V2)
-// PORTD
-#define OV_CTRLW	PORTD
-#define OV_CTRLR	PIND
-#define OV_CTRLD	DDRD
+// PORTC
+#define OV_CTRLW	PORTC
+#define OV_CTRLR	PINC
+#define OV_CTRLD	DDRC
 #define	OV_SCL		_BV(0)
 #define OV_SDA		_BV(1)
 #define OV_VSYNC	_BV(2)
@@ -14,10 +14,10 @@
 #define OV_RRST		_BV(5)
 #define OV_OE		_BV(6)
 #define OV_RCLK		_BV(7)
-// PORTB
-#define OV_DATAW	PORTB
-#define OV_DATAR	PINB
-#define OV_DATAD	DDRB
+// PORTA
+#define OV_DATAW	PORTA
+#define OV_DATAR	PINA
+#define OV_DATAD	DDRA
 
 // I2C
 #define I2C_PORT	OV_CTRLW
