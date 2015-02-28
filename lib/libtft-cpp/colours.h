96fb0456634f2a680ceef0a671c3d3dcd4db0ed4
Norman Zhi <yz39g13@soton.ac.uk>
2015-02-28 21:30:58 +0000

    LIBTFT-CPP Standard colours

---
+++ lib/libtft-cpp/colours.h
@@ -0,0 +1,16 @@
+#ifndef COLOURS_H
+#define COLOURS_H
+
+namespace colour
+{
+	namespace b16
+	{
+		enum Colours {Red = 0xF800, Green = 0x07E0, Blue = 0x001F, Yellow = 0xFFE0, Cyan = 0x07FF, Magenta = 0xF81F};
+	}
+	namespace b32
+	{
+		enum Colours {Red = 0xFF0000, Green = 0x00FF00, Blue = 0x0000FF, Yellow = 0xFFFF00, Cyan = 0x00FFFF, Magenta = 0xFF00FF};
+	}
+}
+
+#endif
