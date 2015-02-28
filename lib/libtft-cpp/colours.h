9ae7f85f865cf869436e1b42877ca8b9052bcc7d
Norman Zhi <yz39g13@soton.ac.uk>
2015-02-28 21:34:49 +0000

    LIBTFT-CPP Colours

--- lib/libtft-cpp/colours.h
+++ lib/libtft-cpp/colours.h
@@ -3,13 +3,17 @@
 
 namespace colour
 {
+	namespace b4
+	{
+		enum Colours {Red = 0x04, Green = 0x02, Blue = 0x01, Yellow = Red | Green, Cyan = Green | Blue, Magenta = Red | Blue};
+	}
 	namespace b16
 	{
-		enum Colours {Red = 0xF800, Green = 0x07E0, Blue = 0x001F, Yellow = 0xFFE0, Cyan = 0x07FF, Magenta = 0xF81F};
+		enum Colours {Red = 0xF800, Green = 0x07E0, Blue = 0x001F, Yellow = Red | Green, Cyan = Green | Blue, Magenta = Red | Blue};
 	}
 	namespace b32
 	{
-		enum Colours {Red = 0xFF0000, Green = 0x00FF00, Blue = 0x0000FF, Yellow = 0xFFFF00, Cyan = 0x00FFFF, Magenta = 0xFF00FF};
+		enum Colours {Red = 0xFF0000, Green = 0x00FF00, Blue = 0x0000FF, Yellow = Red | Green, Cyan = Green | Blue, Magenta = Red | Blue};
 	}
 }
 
