846f8473e6da59cda32dc6b2295e6ec029234169
Norman Zhi <yz39g13@soton.ac.uk>
2015-02-28 21:14:36 +0000

    Directory structure changes fix

--- lib/libcommon/adc.h
+++ lib/libcommon/adc.h
@@ -1,11 +1,19 @@
 #ifndef ADC_H
 #define ADC_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <avr/io.h>
 
 void adc_init(uint8_t channel);
 void adc_enable(const uint8_t e);
 void adc_start(void);
 uint16_t adc_read(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
