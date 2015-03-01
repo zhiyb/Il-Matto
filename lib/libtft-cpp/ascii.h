b296af48a93fdf6b1303baa83c2671f4e63f8ed5
Norman Zhi <yz39g13@soton.ac.uk>
2015-03-01 02:18:06 +0000

    LIBTFT-CPP ascii.cpp changed to ascii.c

--- lib/libtft-cpp/ascii.h
+++ lib/libtft-cpp/ascii.h
@@ -1,8 +1,19 @@
 #ifndef ASCII_H
 #define ASCII_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FONT_WIDTH 6
+#define FONT_HEIGHT 8
+
 #include <avr/pgmspace.h>
 
 extern const unsigned char ascii[96][8] PROGMEM;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
