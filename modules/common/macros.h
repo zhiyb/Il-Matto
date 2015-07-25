a943427864c78cf80a4c5ca5ec7b1698ac6e3991
Norman Zhi <yz39g13@soton.ac.uk>
2015-07-25 14:03:39 +0800

    Fix for data type width in colour macros

--- modules/common/macros.h
+++ modules/common/macros.h
@@ -5,15 +5,19 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+#include <stdint.h>
+
 #define CONCAT(a,b)	a ## b
 #define CONCAT_E(a,b)	CONCAT(a, b)
 
 #define STRINGIFY(x)	#x
 #define TOSTRING(x)	STRINGIFY(x)
 
-#define COLOUR888(r, g, b)	(((r) << 16) | ((g) << 8) | ((b) << 0))
-#define RED888(r)		(((r) >> 16) & 0xff)
-#define GREEN888(g)		(((g) >> 8) & 0xff)
-#define BLUE888(b)		(((b) >> 0) & 0xff)
+#define COLOUR888(r, g, b)	(((uint32_t)(r) << 16) | \
+				((uint32_t)(g) << 8) | \
+				((uint32_t)(b) << 0))
+#define RED888(r)		(((uint32_t)(r) >> 16) & 0xff)
+#define GREEN888(g)		(((uint32_t)(g) >> 8) & 0xff)
+#define BLUE888(b)		(((uint32_t)(b) >> 0) & 0xff)
 
 #endif
