f88843936b66841d29f10e62086ee4565ca2b8ce
Norman Zhi <yz39g13@soton.ac.uk>
2015-10-13 17:03:44 +0100

    Colour macros

--- modules/common/macros.h
+++ modules/common/macros.h
@@ -15,21 +15,24 @@
 #define STRINGIFY(x)	#x
 #define TOSTRING(x)	STRINGIFY(x)
 
-// Combine to 32-bit RGB-888
+// Combine RGB components
 #define COLOUR_888(r, g, b)	((((uint32_t)(r) & 0xff) << 16) | \
 				(((uint32_t)(g) & 0xff) << 8) | \
 				(((uint32_t)(b) & 0xff) << 0))
+#define COLOUR_565(r, g, b)	((((uint16_t)(c) & 0xf8) << 8) | \
+				(((uint16_t)(c) & 0xfc) << 3) | \
+				(((uint16_t)(c) & 0xf8) >> 3))
 // Convert between RGB-888 and RGB-565
-#define COLOUR_565_888(c)	((((uint32_t)(c) & 0x00f80000) >> 8) + \
-				(((uint32_t)(c) & 0x0000fc00) >> 5) + \
+#define COLOUR_565_888(c)	((((uint32_t)(c) & 0x00f80000) >> 8) | \
+				(((uint32_t)(c) & 0x0000fc00) >> 5) | \
 				(((uint32_t)(c) & 0x000000f8) >> 3))
-#define COLOUR_888_565(c)	()
+// TODO #define COLOUR_888_565(c)	()
 // Invert RGB & BGR
-#define COLOUR_888_I(c)	((((uint32_t)(c) & 0x00ff0000) >> 16) + \
-			((uint32_t)(c) & 0x0000ff00) + \
+#define COLOUR_888_I(c)	((((uint32_t)(c) & 0x00ff0000) >> 16) | \
+			((uint32_t)(c) & 0x0000ff00) | \
 			(((uint32_t)(c) & 0x000000ff) << 16))
-#define COLOUR_565_I(c)	(((((uint16_t)(c) & 0xf800) >> 11) + \
-			((uint16_t)(c) & 0x07e0) + \
+#define COLOUR_565_I(c)	(((((uint16_t)(c) & 0xf800) >> 11) | \
+			((uint16_t)(c) & 0x07e0) | \
 			(((uint16_t)(c) & 0x001f) << 11))
 // Extract RGB components
 #define RED_888(c)	(((uint32_t)(c) >> 16) & 0xff)
