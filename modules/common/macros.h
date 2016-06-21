70ecf5ee9774fa4f34985c291be0af676739572e
Norman Zhi <normanzyb@gmail.com>
2016-06-21 17:12:50 +0100

    common: macros.h STATIC_ASSERT

--- modules/common/macros.h
+++ modules/common/macros.h
@@ -15,6 +15,25 @@
 #define STRINGIFY(x)	#x
 #define TOSTRING(x)	STRINGIFY(x)
 
+#define ARRAY_SIZE(a)	(sizeof((a)) / sizeof((a)[0]))
+#define ALIGNED(b)	__attribute__((aligned(b)))
+
+// http://www.pixelbeat.org/programming/gcc/static_assert.html
+#define ASSERT_CONCAT_(a, b) a##b
+#define ASSERT_CONCAT(a, b) ASSERT_CONCAT_(a, b)
+/* These can't be used after statements in c89. */
+#ifdef __COUNTER__
+  #define STATIC_ASSERT(e,m) \
+    ;enum { ASSERT_CONCAT(static_assert_, __COUNTER__) = 1/(int)(!!(e)) }
+#else
+  /* This can't be used twice on the same line so ensure if using in headers
+   * that the headers are not included twice (by wrapping in #ifndef...#endif)
+   * Note it doesn't cause an issue when used on same line of separate modules
+   * compiled with gcc -combine -fwhole-program.  */
+  #define STATIC_ASSERT(e,m) \
+    ;enum { ASSERT_CONCAT(assert_line_, __LINE__) = 1/(int)(!!(e)) }
+#endif
+
 // Combine RGB components
 #define COLOUR_888(r, g, b)	((((uint32_t)(r) & 0xff) << 16) | \
 				(((uint32_t)(g) & 0xff) << 8) | \
